03035574c5671456a84aae132c613ec2c9fb185c
minghaoqi <haoz1w0w@126.com>
2017-03-23 10:56:39 +0800

    1111

---
+++ my-firsts/src/main/java/com/first/entity/customerBean.java
@@ -0,0 +1,30 @@
+package com.first.entity;
+
+import java.io.Serializable;
+
+import sun.util.resources.CurrencyNames_is_IS;
+
+public class customerBean implements Serializable {
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String  customerName;
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	
+	public static void main(String[] args) {
+		customerBean c = new customerBean();
+		c.setCustomerName("111");
+		System.out.println(c.getCustomerName());
+	}
+	
+}
--- my-firsts/src/main/webapp/index.jsp
+++ my-firsts/src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>Hello Worldssssssssssss!</h2>
 </body>
 </html>
