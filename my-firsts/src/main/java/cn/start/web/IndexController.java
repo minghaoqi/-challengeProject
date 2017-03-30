4b5837ced2fb88de97d9152ca7a5c47921353f45
caoxudong <1106388574@qq.com>
2017-03-30 09:44:52 +0800

    修改shiro错误

--- my-firsts/src/main/java/cn/start/web/IndexController.java
+++ my-firsts/src/main/java/cn/start/web/IndexController.java
@@ -3,6 +3,8 @@ package cn.start.web;
 import cn.start.service.UserService;
 import cn.start.utils.RequestResult;
 import cn.start.utils.ResultInfo;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.apache.shiro.authz.annotation.RequiresUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
