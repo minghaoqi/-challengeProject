293f16958d4b73023ab360cceecf975f94ce56ac
GengHaoChen <GengHaoChenLove@Gmail.com>
2017-04-29 23:16:46 +0800

    update

--- forum/forum-web/forum/index.js
+++ forum/forum-web/forum/index.js
@@ -1,17 +1,10 @@
-var mainWarp = angular.module('mainWrap', ['ngRoute']);
-mainWarp
-    .controller('signController', function ($scope, $route) {
-        $scope.$route = $route;
-    }).controller("groupController", function ($scope, $route) {
-    $scope.$route = $route;
-}).config(function ($routeProvider) {
-    $routeProvider
-        .when('/signIn', {
-            templateUrl: '../sign/sign-in.html?itemTitle=1',
-            controller: 'signController'
-        })
-        .when("/group", {
-            templateUrl: '../group/group.html',
-            controller: 'groupController'
-        })
+var mainWarp = angular.module('mainWrap', ['ui.router']);
+mainWarp.config(function ($stateProvider) {
+    $stateProvider.state = {
+
+
+
+
+
+    }
 });
--- forum/forum-web/group/group.js
+++ forum/forum-web/group/group.js
@@ -1,6 +1,6 @@
 var groupApp = angular.module("groupApp", []);
 groupApp.controller('groupAppLoad', function ($scope, $http) {
     $http.get("http://127.0.0.1:8081/forum/item/itemTitles").then(function (response) {
-        $scope.groupData = response.data;
+        $scope.topics = response.data;
     })
 });
