5b9ecd1865c28aa149a816de8c4f205aa90b84c1
GengHaoChen <GengHaoChenLove@Gmail.com>
2017-04-30 20:12:29 +0800

    topic

--- forum/forum-web/forum/index.js
+++ forum/forum-web/forum/index.js
@@ -1,18 +1,18 @@
 var mainWarp = angular.module('mainWrap', ['ui.router']);
+mainWarp.controller('groupLoadController', function ($scope, $http) {
+    $http.get('http://127.0.0.1:8081/forum/item/itemTitles').then(function (response) {
+        $scope.topics = response.data;
+    })
+});
 mainWarp.config(function ($stateProvider) {
     $stateProvider.state('signIn', {
-        url: '/signIn',
+        url: '/login',
         templateUrl: '../sign/sign-in.html'
     });
     $stateProvider.state('group', {
-        url: '/group',
-        templateUrl: '../group/group.html',
-        component: 'group',
-        resolve: {
-            group: function (groupService) {
-                return groupService.getGroupTopics();
-            }
-
+            url: '/group',
+            templateUrl: '../group/group.html',
+            controller: 'groupLoadController'
         }
-    })
+    )
 });
