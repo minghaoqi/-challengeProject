880c9e5fe4de70697c2991de1a14b64fd61e73cc
GengHaoChen <GengHaoChenLove@Gmail.com>
2017-05-01 22:45:04 +0800

    ......

--- forum/forum-web/forum/index.js
+++ forum/forum-web/forum/index.js
@@ -1,13 +1,8 @@
 var mainWarp = angular.module('mainWrap', ['ui.router']);
-mainWarp.controller('groupLoadController', function ($scope, $http) {
+mainWarp.controller('groupLoadController', function ($scope, $http, $state) {
     $http.get('http://127.0.0.1:8081/forum/item/topics').then(function (response) {
         $scope.topics = response.data;
-    })
-});
-mainWarp.controller('toArticleListController', function ($scope, $http, $state) {
-    $scope.toProducer = function (itemTopicId, itemTopic) {
-        $state.go('article-list', {itemTopicId: itemTopicId, itemTopic: itemTopic});
-    };
+    });
 });
 mainWarp.controller('articleListController', function ($scope, $stateParams) {
     $scope.topicTitle = $stateParams.topicTitle;
@@ -26,9 +21,8 @@ mainWarp.config(function ($stateProvider) {
         controller: 'groupLoadController'
     });
     $stateProvider.state('article-list', {
-        url: 'article-list/:topicId',
+        url: 'article-list/:itemTopicId',
         templateUrl: '../article-list/article-list.html',
         controller: 'articleListController'
     });
-
 });
